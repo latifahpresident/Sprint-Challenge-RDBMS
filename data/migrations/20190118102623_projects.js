a9cf9ccfcb772b4a229a4f0b5bfec4bc945d0180
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-01-18 10:40:43 -0600

    projects table created

--- data/migrations/20190118102623_projects.js
+++ data/migrations/20190118102623_projects.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('projects', tbl => {
+      tbl.increments()
+      tbl.string('name', 128)
+      tbl.string('description', 128)
+      tbl.boolean('completed').defaultTo(false)
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('projects');
 };
