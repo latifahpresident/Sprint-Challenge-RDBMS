e502d1fb9f31c2db286539b916844e31702150de
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-01-18 10:36:38 -0600

    actions table added

--- data/migrations/20190118102157_actions.js
+++ data/migrations/20190118102157_actions.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('actions', tbl => {
+      tbl.increments()
+      tbl.string('description', 128).notNullable()
+      tbl.text('notes').notNullable()
+      tbl.boolean('completed').defaultTo(false)
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('actions');
 };
